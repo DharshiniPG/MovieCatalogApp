173d3bf3e18996e0ecd1fc5b9cf5188c2ae81ce3
DharshiniPG <dhachugopi26@gmail.com>
2020-11-20 09:16:01 +0530

    Changes made in FragmentOne

--- app/src/main/java/com/example/moviecatalogapp/fragments/FragmentOne.java
+++ app/src/main/java/com/example/moviecatalogapp/fragments/FragmentOne.java
@@ -1,38 +1,19 @@
 package com.example.moviecatalogapp.fragments;
 
-import android.content.Context;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.example.moviecatalogapp.R;
-import com.example.moviecatalogapp.adapter.MoviesListAdapter;
 import com.example.moviecatalogapp.databinding.FragmentOneBinding;
-import com.example.moviecatalogapp.modal.Movies;
-import com.example.moviecatalogapp.services.ApiClient;
-import com.example.moviecatalogapp.services.ApiService;
 import com.example.moviecatalogapp.viewmodal.FragmentOneViewModal;
-import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 import org.jetbrains.annotations.NotNull;
-
-import java.lang.reflect.Type;
-import java.util.List;
-import java.util.Objects;
-
-import io.reactivex.Observable;
-import io.reactivex.Observer;
-import io.reactivex.android.schedulers.AndroidSchedulers;
-import io.reactivex.disposables.Disposable;
-import io.reactivex.schedulers.Schedulers;
 
 public class FragmentOne extends Fragment{
 
